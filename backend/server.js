7dfeebea72b3007d7185c3dc92455e112d7ac2db
ShaniceTang <e0959739@u.nus.edu>
2023-07-27 21:03:36 +0800

    jhkhhg

--- backend/server.js
+++ backend/server.js
@@ -14,25 +14,27 @@ app.use(express.json())
 
 //cors
 //const allowedOrigins = ['https://frontend-modstop.onrender.com/', "http://localhost:3000/"];
-const isProduction = process.env.NODE_ENV === 'production';
+//const isProduction = process.env.NODE_ENV === 'production';
 
-const allowedOrigins = isProduction ? ['https://frontend-modstop.onrender.com/'] : true;
+//const allowedOrigins = isProduction ? ['https://frontend-modstop.onrender.com/'] : true;
 
 
-const corsOptions = {
-  origin: allowedOrigins,
-//   (origin, callback) => {
-//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: allowedOrigins,
+// //   (origin, callback) => {
+// //     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+// //       callback(null, true);
+// //     } else {
+// //       callback(new Error('Not allowed by CORS'));
+// //     }
+// //   },
+//   credentials: true,
+//   optionsSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: ["http://localhost:3000"]
+}));
 
 //will fire everytime a new request comes in
 app.use((req, res, next)=>{
